98cff025efe0b609180c43835f2718eaaaddabda
Angelo <Angelodimauro91@gmail.com>
2022-09-25 22:47:16 +0200

    Bottone (Footer profondo)

--- boolean-es/src/assets/footer_profondo.vue
+++ boolean-es/src/assets/footer_profondo.vue
@@ -1,7 +1,9 @@
 <template>
-    <div>
-        ciao
-    </div>
+    <section id="SecOne">
+        <div class="container">
+            <input type="submit" value="SIGN-UP NOW!">
+        </div>
+    </section>
 </template>
 
 <script>
@@ -11,5 +13,23 @@
 </script>
 
 <style lang="scss" scoped>
-
+    #SecOne{
+        background-color: hsl(0deg 0% 19%);
+        width: 100%;
+        height: 13vh;
+    }
+    .container{
+        width: 80%;
+        
+        margin: auto;
+        display: flex;
+    
+        input{
+            padding: 17px;
+            background-color: rgba(255, 0, 0, 0);
+            border: 2px solid hsl(209deg 98% 49%);
+            color: white;
+            margin-top: 35px;
+        }
+    }
 </style>
