6ef77be8a3ef2728ddb5cc84e4b16c49f818dcdf
Angelo <Angelodimauro91@gmail.com>
2022-09-25 20:10:38 +0200

    Blocco lista

--- boolean-es/src/assets/footer_one.vue
+++ boolean-es/src/assets/footer_one.vue
@@ -1,6 +1,53 @@
 <template>
     <div>
         <section id="sectionOne">
+
+            <div class="ListaInfo">
+                <h4>DC COMICS</h4>
+                <ul>
+                    <li><a href=""></a></li>
+                    <li><a href=""></a></li>
+                    <li><a href=""></a></li>
+                    <li><a href=""></a></li>
+                    <li><a href=""></a></li>
+                    <li><a href=""></a></li>
+                    <li><a href=""></a></li>
+                </ul>
+
+                <h4>DC COMICS</h4>
+                <ul>
+                    <li><a href=""></a></li>
+                    <li><a href=""></a></li>
+                    <li><a href=""></a></li>
+                    <li><a href=""></a></li>
+                    <li><a href=""></a></li>
+                    <li><a href=""></a></li>
+                    <li><a href=""></a></li>
+                </ul>
+
+                <h4>DC COMICS</h4>
+                <ul>
+                    <li><a href=""></a></li>
+                    <li><a href=""></a></li>
+                    <li><a href=""></a></li>
+                    <li><a href=""></a></li>
+                    <li><a href=""></a></li>
+                    <li><a href=""></a></li>
+                    <li><a href=""></a></li>
+                </ul>
+
+                <h4>DC COMICS</h4>
+                <ul>
+                    <li><a href=""></a></li>
+                    <li><a href=""></a></li>
+                    <li><a href=""></a></li>
+                    <li><a href=""></a></li>
+                    <li><a href=""></a></li>
+                    <li><a href=""></a></li>
+                    <li><a href=""></a></li>
+                </ul>
+            </div>
+
             <div class="logoDcTrasparente">
                 <img src="../assets/img/dc-logo-bg.png" alt="">
             </div>
@@ -21,14 +68,27 @@ export default{
         width: 100%;
         height: 50vh;
         display: flex;
-        justify-content: end;
-        position: relative;
+        justify-content: space-around;
+        
 
          .logoDcTrasparente img{   
-             width: 25%;
+             width: 100%;
              height: 100%;
-             position: absolute;
-             right: 10%;
+             position: relative;
+            right: 20%;
          }
+    }
+
+    .ListaInfo{
+        display: flex;
+        flex-wrap: wrap;
+        width: 40%;
+        height: 100%;
+        background-color: rgba(255, 255, 255, 0.767);
+        ul{
+            li{
+                list-style: none;
+            }
+        }
     }
 </style>
